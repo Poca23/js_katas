fa23bb737e33a7a12f15c7aa051b956e8e1d3b26
pilou <p-l.b@hotmail.com>
2024-09-25 08:56:58 +0200

    ended order kata-2

---
+++ kata-2.js
@@ -0,0 +1,18 @@
+/**
+ * 1 - Écris une fonction sumOfEvenNumbers(numbers) qui prend en entrée un tableau d'entiers numbers et retourne la somme de tous les nombres pairs présents dans le tableau.
+ */
+
+const numbers = [1, 2, 3, 4, 5, 6];
+
+// your code here
+
+
+
+
+/**
+ * 2 - Écris une fonction maxNumber(numbers) qui prend en entrée un tableau d'entiers numbers et retourne le plus grand nombre présent dans le tableau.
+ */
+
+const numbers2 = [12, 21, 67, 36, 49, 51];
+
+// your code here
