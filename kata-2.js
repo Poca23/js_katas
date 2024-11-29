a517869b05870e9ee49be223a2f4a1820452cf9a
Claire Naudin <cndweb37@gmail.com>
2024-11-29 10:23:31 +0100

    solvedExo1KataJs

--- kata-2.js
+++ kata-2.js
@@ -3,11 +3,19 @@
  */
 
 const numbers = [1, 2, 3, 4, 5, 6];
+let sumNb = 0;
+const nbPair = numbers.filter((element) => element % 2 === 0);
+console.log(nbPair);
+let nbPairSum = 0;
 
-// your code here
-
-
-
+for (i = 0; i < nbPair.length; i++) {
+  i += nbPair[i];
+  const nbPairSum = nbPair.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    sumNb
+  );
+  console.log(nbPairSum);
+}
 
 /**
  * 2 - Écris une fonction maxNumber(numbers) qui prend en entrée un tableau d'entiers numbers et retourne le plus grand nombre présent dans le tableau.
